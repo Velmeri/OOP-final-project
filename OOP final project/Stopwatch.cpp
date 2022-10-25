373cd74263fd2a2575e844d403a0c08e56625025
Velmeri <danielmikhh2gmail.com>
2022-10-25 21:35:11 +0200

    added class Stopwatch

--- OOP final project/Stopwatch.cpp
+++ OOP final project/Stopwatch.cpp
@@ -1,1 +1,6 @@
 #include "Stopwatch.h"
+
+int Stopwatch::GetTime() const
+{
+	return time(0) - start;
+}
--- OOP final project/Stopwatch.h
+++ OOP final project/Stopwatch.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <time.h>
+
 class Stopwatch
 {
+	time_t start;
+public:
+	Stopwatch() { start = time(0); }
+
+	int GetTime() const;
 };
 
